926ef1113d0858f2f8d11b45f6bfa0dc14ac8686
PC <truongvanhao159@gmail.com>
2023-10-23 11:11:57 +0700

    Done api get post put delete - tour guide

--- src/main/java/com/tourmanagement/Controllers/TourGuideController.java
+++ src/main/java/com/tourmanagement/Controllers/TourGuideController.java
@@ -1,13 +1,42 @@
 package com.tourmanagement.Controllers;
 
+import com.tourmanagement.DTOs.TourGuideDTO;
+import com.tourmanagement.Models.Tour;
+import com.tourmanagement.Models.TourGuide;
 import com.tourmanagement.Services.TourGuideService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-@RestController()
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/tour-guides")
+@ResponseStatus(HttpStatus.OK)
 public class TourGuideController {
     private final TourGuideService tourGuideService;
 
     public TourGuideController(TourGuideService tourGuideService) {
         this.tourGuideService = tourGuideService;
+    }
+
+    @GetMapping("/{id}")
+    public TourGuide handleGetSpecificTourGuide(@PathVariable Long id) {
+        TourGuide tourGuide = tourGuideService.getTourGuideById(id);
+
+        return tourGuide;
+    }
+
+    @PostMapping()
+    public TourGuide handleCreateNewTourGuide(@RequestBody @Valid TourGuideDTO tourGuideDTO) {
+        TourGuide newTourGuide = tourGuideService.createNewTourGuide(tourGuideDTO);
+
+        return newTourGuide;
+    }
+
+    @PutMapping("/{id}")
+    public TourGuide handleUpdateExistedTourGuide(@PathVariable Long id, @RequestBody @Valid TourGuideDTO tourGuideDTO) {
+        TourGuide updatedTourGuide = tourGuideService.updateTourGuide(id, tourGuideDTO);
+
+        return updatedTourGuide;
     }
 }
